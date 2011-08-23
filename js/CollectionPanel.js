cb57d119da22d2f783c23e316a6d742b8ee94ac6
Nigel Banks <nigel.g.banks@gmail.com>
2011-08-23 14:04:49 +0100

    Search filter is working but hard coded, The sort fields are also working but again hard coded.

--- js/CollectionPanel.js
+++ js/CollectionPanel.js
@@ -1,45 +1,91 @@
 Ext.onReady(function(){
-    Ext.define('ContentModelViewer.widgets.CollectionPanel', {
-        extend: 'Ext.panel.Panel',
-        itemId: 'collection',
-        title: 'Collection',
-        dockedItems: [{
-            xtype: 'toolbar',
-            dock: 'top',
-            items: []
-        }, {
-            id: 'collection-pager',
-            xtype: 'pagingtoolbar',
-            store: Ext.data.StoreManager.lookup('members'),   // same store GridPanel is using
-            dock: 'bottom',
-            displayInfo: true
-        }],
-        items: [{
-            xtype: 'dataview',
-            store: Ext.data.StoreManager.lookup('members'),
-            itemSelector: 'div.file-item',
-            emptyText: 'No Files Available',
-            deferEmptyText: false,
-            itemTpl: new Ext.XTemplate(
-                '<tpl for=".">',
-                '   <div class="member-item">',
-                '       <a href="{link}">',
-                '           <img class="member-item-img" src="{tn}"></img>',
-                '       </a>',
-                '       <a href="{link}">',
-                '       <h2 class="member-item-label">{[fm.ellipsis(this.getLabel(values.label), 100, true)]}</h2>',
-                '       </a>',
-                '       <p class="member-item-description">{[fm.ellipsis(values.description, 350, true)]}</p>',
-                '   </div>',
-                '</tpl>',
-                {
-                    compiled: true,
-                    disableFormats: true,
-                    getLabel: function(label) {
-                        var empty = jQuery.trim(label) == '';
-                        return empty ? 'Default Label: (Please notify an administrator to provide a label)' : label;
-                    }
-                })
-        }]
-    });
+  Ext.define('ContentModelViewer.widgets.CollectionPanel', {
+    extend: 'Ext.panel.Panel',
+    itemId: 'collection',
+    title: 'Collection',
+    dockedItems: [{
+      xtype: 'toolbar',
+      dock: 'top',
+      items: [ {
+        xtype: 'tbtext', 
+        text: 'Sort By: '
+      }, {
+        xtype: 'sortbutton',
+        text : 'Label',
+        listeners: {
+          changedirection: function(direction) {
+            var store = Ext.data.StoreManager.lookup('members');
+            var sorters = store.sorters;
+            var title = sorters.get('label');
+            title.setDirection(direction);
+          }
+        }
+      }, {
+        xtype: 'sortbutton',
+        text : 'Created',
+        listeners: {
+          changedirection: function(direction) {
+            var store = Ext.data.StoreManager.lookup('members');
+            var sorters = store.sorters;
+            var date = sorters.get('created');
+            date.setDirection(direction);
+          }
+        }
+      }, {
+        xtype: 'tbtext',
+        text: 'Search'
+      }, {
+        xtype: 'textfield',
+        hideLabel: true
+      }, {
+        xtype: 'button',
+        text: 'Go',
+        handler: function() {
+          var store = Ext.data.StoreManager.lookup('members');
+          var filters = store.filters;
+          var label = filters.get(0);
+          label.value = 'tset';
+        }
+      }]
+    },  {
+      id: 'collection-pager-top',
+      xtype: 'pagingtoolbar',
+      store: Ext.data.StoreManager.lookup('members'),   // same store GridPanel is using
+      dock: 'top',
+      displayInfo: true
+    }, {
+      id: 'collection-pager-bottom',
+      xtype: 'pagingtoolbar',
+      store: Ext.data.StoreManager.lookup('members'),   // same store GridPanel is using
+      dock: 'bottom', 
+      displayInfo: true
+    }],
+    items: [{
+      xtype: 'dataview',
+      store: Ext.data.StoreManager.lookup('members'),
+      itemSelector: 'div.file-item',
+      emptyText: 'No Files Available',
+      deferEmptyText: false,
+      itemTpl: new Ext.XTemplate(
+        '<tpl for=".">',
+        '   <div class="member-item">',
+        '       <a href="{link}">',
+        '           <img class="member-item-img" src="{tn}"></img>',
+        '       </a>',
+        '       <a href="{link}">',
+        '       <h2 class="member-item-label">{[fm.ellipsis(this.getLabel(values.label), 100, true)]}</h2>',
+        '       </a>',
+        '       <p class="member-item-description">{[fm.ellipsis(values.description, 350, true)]}</p>',
+        '   </div>',
+        '</tpl>',
+        {
+          compiled: true,
+          disableFormats: true,
+          getLabel: function(label) {
+            var empty = jQuery.trim(label) == '';
+            return empty ? 'Default Label: (Please notify an administrator to provide a label)' : label;
+          }
+        })
+    }]
+  });
 });
--- js/IncludeFirst.js
+++ js/IncludeFirst.js
@@ -10,217 +10,230 @@ Ext.ns('ContentModelViewer.widgets');
  * Grab properties from properties stored in the HTML 
  */
 Ext.onReady(function(){
-    /**
+  /**
      * Set Properties
      */
-    ContentModelViewer.properties.pid = $('#pid').text();
-    ContentModelViewer.properties.dsid = $('#dsid').text();
-    ContentModelViewer.properties.viewFunction = $('#view_function').text();
-    ContentModelViewer.properties.url = {
-        object: {
-            overview: $('#object_overview_url').text(),
-            properties: $('#object_properties_url').text(),
-            datastreams: $('#object_datastreams_url').text(),
-            members: $('#object_members_url').text(),
-            purge: $('#object_purge_url').text()
-        },
-        datastream: {
-            add: $('#datastream_add_url').text(),
-            purge: function(dsid) {
-                var url = $('#datastream_purge_url').text();
-                return url.replace('/dsid/', '/'+dsid+'/');
-            },
-            properties: function(dsid) {
-                var url = $('#datastream_add_url').text();
-                return url.replace('/dsid/', '/'+dsid+'/');
-            },
-            download: function(dsid) {
-                var url = $('#datastream_download_url').text();
-                return url.replace('/dsid/', '/'+dsid+'/');
-            },
-            view: function(dsid) {
-                var url = $('#datastream_view_url').text();
-                return url.replace('/dsid/', '/'+dsid+'/');
-            }
-        }
-    };
-    /**
+  ContentModelViewer.properties.pid = $('#pid').text();
+  ContentModelViewer.properties.dsid = $('#dsid').text();
+  ContentModelViewer.properties.viewFunction = $('#view_function').text();
+  ContentModelViewer.properties.url = {
+    object: {
+      overview: $('#object_overview_url').text(),
+      properties: $('#object_properties_url').text(),
+      datastreams: $('#object_datastreams_url').text(),
+      members: $('#object_members_url').text(),
+      purge: $('#object_purge_url').text()
+    },
+    datastream: {
+      add: $('#datastream_add_url').text(),
+      purge: function(dsid) {
+        var url = $('#datastream_purge_url').text();
+        return url.replace('/dsid/', '/'+dsid+'/');
+      },
+      properties: function(dsid) {
+        var url = $('#datastream_add_url').text();
+        return url.replace('/dsid/', '/'+dsid+'/');
+      },
+      download: function(dsid) {
+        var url = $('#datastream_download_url').text();
+        return url.replace('/dsid/', '/'+dsid+'/');
+      },
+      view: function(dsid) {
+        var url = $('#datastream_view_url').text();
+        return url.replace('/dsid/', '/'+dsid+'/');
+      }
+    }
+  };
+  /**
      * Define Functions
      */
-    ContentModelViewer.functions = {
-        selectDatastreamRecord: function(record) {
-            ContentModelViewer.properties.dsid = record.get('view');
-            ContentModelViewer.properties.viewFunction = record.get('view_function');
-        },
-        viewSelectedDatastreamRecord: function() {
-            var viewer = Ext.getCmp('datastream-viewer');
-            var loader = viewer.getLoader();
-            loader.load({
-                url: ContentModelViewer.properties.url.datastream.view(ContentModelViewer.properties.dsid)
-            });
-            var viewerPanel = viewer.up('panel');
-            var tabpanel = viewer.up('tabpanel');
-            tabpanel.setActiveTab(viewerPanel);
-        },
-        callDatastreamViewFunction: function() {
-            var pid = ContentModelViewer.properties.pid;
-            var dsid = ContentModelViewer.properties.dsid;
-            var view_function = ContentModelViewer.properties.viewFunction;
-            if(view_function) {
-                eval(view_function)(pid, dsid);
-            }
-        }
+  ContentModelViewer.functions = {
+    selectDatastreamRecord: function(record) {
+      ContentModelViewer.properties.dsid = record.get('view');
+      ContentModelViewer.properties.viewFunction = record.get('view_function');
+    },
+    viewSelectedDatastreamRecord: function() {
+      var viewer = Ext.getCmp('datastream-viewer');
+      var loader = viewer.getLoader();
+      loader.load({
+        url: ContentModelViewer.properties.url.datastream.view(ContentModelViewer.properties.dsid)
+      });
+      var viewerPanel = viewer.up('panel');
+      var tabpanel = viewer.up('tabpanel');
+      tabpanel.setActiveTab(viewerPanel);
+    },
+    callDatastreamViewFunction: function() {
+      var pid = ContentModelViewer.properties.pid;
+      var dsid = ContentModelViewer.properties.dsid;
+      var view_function = ContentModelViewer.properties.viewFunction;
+      if(view_function) {
+        eval(view_function)(pid, dsid);
+      }
     }
-    /**
+  }
+  /**
      * Define Models.
      */
-    Ext.define('ContentModelViewer.models.FedoraObject', {
-        extend: 'Ext.data.Model',
-        fields: [{
-            name: 'link',  
-            type: 'string'
-        }, {
-            name: 'label',  
-            type: 'string'
-        }, {
-            name: 'description',   
-            type: 'string'
-        }, {
-            name: 'owner', 
-            type: 'string'
-        }, {
-            name: 'created', 
-            type: 'string'
-        }, {
-            name: 'modified', 
-            type: 'string'
-        }, {
-            name: 'tn',
-            type: 'string'
-        }]
-    });
-    Ext.define('ContentModelViewer.models.ObjectProperties', {
-        extend: 'Ext.data.Model',
-        fields: [{
-            name: 'label',  
-            type: 'string'
-        }, {
-            name: 'state',   
-            type: 'string'
-        }, {
-            name: 'owner', 
-            type: 'string'
-        }, {
-            name: 'created', 
-            type: 'string'
-        }, {
-            name: 'modified', 
-            type: 'string'
-        },],
-        validations: [{
-            type: 'inclusion', 
-            field: 'state',
-            list: ['Active', 'Inactive', 'Deleted']
-        }]
-    });
-    Ext.define('ContentModelViewer.models.Datastream', {
-        extend: 'Ext.data.Model',
-        idProperty: 'dsid',
-        fields: [{
-            name: 'dsid',  
-            type: 'string'
-        }, {
-            name: 'label',  
-            type: 'string'
-        }, {
-            name: 'state',   
-            type: 'string'
-        }, {
-            name: 'created', 
-            type: 'string'
-        }, {
-            name: 'mime', 
-            type: 'string'
-        }, {
-            name: 'view', 
-            type: 'string'
-        }, {
-            name: 'download', 
-            type: 'string'
-        }, {
-            name: 'tn', 
-            type: 'string'
-        }, {
-            name: 'view_function', 
-            type: 'string'
-        }],
-        validations: [{
-            type: 'inclusion', 
-            field: 'state',   
-            list: ['A', 'I']
-        }],
-        proxy: {
-            type: 'rest',
-            url : ContentModelViewer.properties.url.object.datastreams,
-            reader: {
-                type: 'json',
-                root: 'data',
-                totalProperty: 'total'
-            }
+  Ext.define('ContentModelViewer.models.FedoraObject', {
+    extend: 'Ext.data.Model',
+    fields: [{
+      name: 'link',  
+      type: 'string'
+    }, {
+      name: 'label',  
+      type: 'string'
+    }, {
+      name: 'description',   
+      type: 'string'
+    }, {
+      name: 'owner', 
+      type: 'string'
+    }, {
+      name: 'created', 
+      type: 'string'
+    }, {
+      name: 'modified', 
+      type: 'string'
+    }, {
+      name: 'tn',
+      type: 'string'
+    }]
+  });
+  Ext.define('ContentModelViewer.models.ObjectProperties', {
+    extend: 'Ext.data.Model',
+    fields: [{
+      name: 'label',  
+      type: 'string'
+    }, {
+      name: 'state',   
+      type: 'string'
+    }, {
+      name: 'owner', 
+      type: 'string'
+    }, {
+      name: 'created', 
+      type: 'string'
+    }, {
+      name: 'modified', 
+      type: 'string'
+    },],
+    validations: [{
+      type: 'inclusion', 
+      field: 'state',
+      list: ['Active', 'Inactive', 'Deleted']
+    }]
+  });
+  Ext.define('ContentModelViewer.models.Datastream', {
+    extend: 'Ext.data.Model',
+    idProperty: 'dsid',
+    fields: [{
+      name: 'dsid',  
+      type: 'string'
+    }, {
+      name: 'label',  
+      type: 'string'
+    }, {
+      name: 'state',   
+      type: 'string'
+    }, {
+      name: 'created', 
+      type: 'string'
+    }, {
+      name: 'mime', 
+      type: 'string'
+    }, {
+      name: 'view', 
+      type: 'string'
+    }, {
+      name: 'download', 
+      type: 'string'
+    }, {
+      name: 'tn', 
+      type: 'string'
+    }, {
+      name: 'view_function', 
+      type: 'string'
+    }],
+    validations: [{
+      type: 'inclusion', 
+      field: 'state',   
+      list: ['A', 'I']
+    }],
+    proxy: {
+      type: 'rest',
+      url : ContentModelViewer.properties.url.object.datastreams,
+      reader: {
+        type: 'json',
+        root: 'data',
+        totalProperty: 'total'
+      }
+    }
+  });
+  /**
+   * Create Stores
+   */
+  Ext.create('Ext.data.Store', {
+    storeId:'members',
+    model: ContentModelViewer.models.FedoraObject,
+    autoLoad: true,
+    autoSync: true,
+    pageSize: 7,
+    remoteSort: true,
+    remoteFilter: true,
+    sorters: [{
+      property : 'label',
+      direction: 'ASC'
+    }, {
+      property : 'created',
+      direction: 'ASC'
+    }],
+    filters: [{
+      property: 'label',
+      value: null
+    }],
+    proxy: {
+      type: 'ajax',
+      url : ContentModelViewer.properties.url.object.members,
+      reader: {
+        type: 'json',
+        root: 'data'
+      }
+    }
+  });
+  Ext.create('Ext.data.Store', {
+    storeId:'datastreams',
+    model: ContentModelViewer.models.Datastream,
+    autoLoad: true,
+    autoSync: true,
+    pageSize: 15,
+    listeners: {
+      write: function(store, operation) {
+        var records = operation.getRecords();
+        if(operation.wasSuccessful()) {
+          for (var i = 0; i < records.length; i++) {
+            var record = records[i];
+            record.commit();
+          }
         }
-    });
-    /**
-     * Create Stores
-     */
-    Ext.create('Ext.data.Store', {
-        storeId:'members',
-        model: ContentModelViewer.models.FedoraObject,
-        autoLoad: true,
-        autoSync: true,
-        pageSize: 7,
-        proxy: {
-            type: 'ajax',
-            url : ContentModelViewer.properties.url.object.members,
-            reader: {
-                type: 'json',
-                root: 'data'
-            }
-        }
-    });
-    Ext.create('Ext.data.Store', {
-        storeId:'datastreams',
-        model: ContentModelViewer.models.Datastream,
-        autoLoad: true,
-        autoSync: true,
-        pageSize: 15,
-        listeners: {
-            write: function(store, operation) {
-                var records = operation.getRecords();
-                if(operation.wasSuccessful()) {
-                    for (var i = 0; i < records.length; i++) {
-                        var record = records[i];
-                        record.commit();
-                    }
-                }
-            }
-        }
-    });
-    Ext.create('Ext.data.Store', {
-        storeId:'files',
-        model: ContentModelViewer.models.Datastream,
-        autoLoad: true,
-        pageSize: 4,
-        proxy: {
-            type: 'rest',
-            url : ContentModelViewer.properties.url.object.datastreams,
-            extraParams: {
-                filter: true
-            },
-            reader: {
-                type: 'json',
-                root: 'data',
-                totalProperty: 'total'
-            }
-        }
-    });
+      }
+    }
+  });
+  Ext.create('Ext.data.Store', {
+    storeId:'files',
+    model: ContentModelViewer.models.Datastream,
+    autoLoad: true,
+    pageSize: 4,
+    proxy: {
+      type: 'rest',
+      url : ContentModelViewer.properties.url.object.datastreams,
+      extraParams: {
+        filter: true
+      },
+      reader: {
+        type: 'json',
+        root: 'data',
+        totalProperty: 'total'
+      }
+    }
+  });
 });
