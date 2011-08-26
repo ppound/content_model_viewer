7e7dc81876359f6e5c982867e57e5289043dac00
Nigel Banks <nigel.g.banks@gmail.com>
2011-08-26 12:11:56 +0100

    Don't want to lose changes before refactoring.

--- AdminSettings.tpl.php
+++ AdminSettings.tpl.php
@@ -1,4 +1,6 @@
+<br/>
 <div id="admin-settings-main">
+  <h3><?php print t('Custom Settings') ?></h3>
   <?php if (count($content_models) > 0) : ?>
     <div id="admin-settings-table">
       <?php $table_rows = array(); ?>
@@ -17,7 +19,11 @@
   <div id="admin-settings-add-form">
     <?php print drupal_get_form('content_model_viewer_add_custom_settings_form'); ?>
   </div>
+  <div id="admin-settings-custom-form">
+    <?php print drupal_get_form('content_model_viewer_custom_settings_form'); ?>
+  </div>
   <div id="admin-settings-default-form">
-    <?php print drupal_get_form('content_model_viewer_default_settings_form'); ?>
+    <h3><?php print t('Default Settings') ?></h3>
+    <?php print drupal_get_form('content_model_viewer_datastream_rules_form'); ?>
   </div>
 </div>
