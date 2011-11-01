86b4b81fbd91ba7da01c2a1506da7ee25544db1d
TJ Lewis <tj@discoverygarden.ca>
2011-11-01 13:48:46 -0300

    First Commit

---
+++ js/TreePanel.js
@@ -0,0 +1,36 @@
+Ext.define('ContentModelViewer.widgets.TreePanel', {
+    extend: 'Ext.tree.Panel',
+    uses: [
+        'Ext.tree.Panel'
+    ],
+    initComponent: function() {
+        Ext.apply(this, {
+            region: 'west', 
+            store:'treemembers',// Ext.data.StoreManager.lookup('treemembers'),
+            autoLoad: false,
+            root: 'data',
+            rootVisible: false,
+            collapsible: true,
+            collapseDirection: 'left',
+            title: 'Projects',
+            width: 200
+        });
+        this.callParent(this);
+    },
+    listeners: {
+      itemdblclick: {
+        fn: function(view, record, item, index, event) {
+            //the record is the data node that was clicked
+            //the item is the html dom element in the tree that was clicked
+            //index is the index of the node relative to its parent
+            nodeId = record.data.id;
+            link = record.data.link;
+            leaf = record.data.leaf;
+            htmlId = item.id;
+            window.location = link;
+        }
+      }
+    }
+});
+
+
