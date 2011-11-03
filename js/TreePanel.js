b89bfc05bb703adb5fbc655f49678668bf4b0c06
TJ Lewis <tj@discoverygarden.ca>
2011-11-03 11:30:25 -0300

    changed to usearrows true

--- js/TreePanel.js
+++ js/TreePanel.js
@@ -12,6 +12,7 @@ Ext.define('ContentModelViewer.widgets.TreePanel', {
     collapseDirection: 'left',
     title: 'Projects',
     width: 200,
+    useArrows: true,
     listeners: {
       itemdblclick: {
         fn: function(view, record, item, index, event) {
@@ -25,7 +26,26 @@ Ext.define('ContentModelViewer.widgets.TreePanel', {
             window.location = link;
         }
       }
-    }
+    },
+    dockedItems: [{
+      xtype: 'toolbar',
+      dock: 'top',
+      items: [
+        {
+          xtype: 'splitbutton',
+          text: 'Actions',
+          tooltip: {text:'This is a an example QuickTip for a toolbar item', title:'Tip Title'},
+          iconCls: 'blist',
+          menu : {
+              items: [{
+                  text: 'View Metadata for Selected'
+              }, {
+                  text: 'Manage Selected'
+              }, {
+                  text: 'Add a new item'
+              }]
+          }
+        }
+      ]
+    }]
 });
-
-
