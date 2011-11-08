2905ba56ee34780f266f1cb83b4050b46733b17d
TJ Lewis <tj@discoverygarden.ca>
2011-11-08 10:57:27 -0400

    changed itemclick

--- js/TreePanel.js
+++ js/TreePanel.js
@@ -14,37 +14,47 @@ Ext.define('ContentModelViewer.widgets.TreePanel', {
     width: 200,
     useArrows: true,
     listeners: {
-      itemdblclick: {
+      itemclick: {
         fn: function(view, record, item, index, event) {
-            //the record is the data node that was clicked
-            //the item is the html dom element in the tree that was clicked
+            //record is the data node that was clicked
+            //item is the html dom element in the tree that was clicked
             //index is the index of the node relative to its parent
-            //nodeId = record.data.id;
+
             link = record.data.link;
-            //leaf = record.data.leaf;
-            //htmlId = item.id;
             window.location = link;
         }
+      },
+      /*itemclick: function(view, record, item, index, e){
+        selected_node = record;
+        if(record) {
+          button = Ext.getCmp('viewer-view-file1');
+          button.enable();
+          //record.get('view') ? button.enable() : button.disable(); 
+        }//end if
+      },*/
+      selectionchange: function(view, selections, options) {
+        var button, record = selections[0];
+        if(record) {
+          button = Ext.getCmp('viewer-view-file1');
+          record.get('id') ? button.enable() : button.disable();
+        }   
       }
     },
     dockedItems: [{
       xtype: 'toolbar',
       dock: 'top',
-      items: [
-        {
-          xtype: 'splitbutton',
-          text: 'Actions',
-          tooltip: {text:'This is a an example QuickTip for a toolbar item', title:'Tip Title'},
-          iconCls: 'blist',
-          menu : {
-              items: [{
-                  text: 'View Metadata for Selected'
-              }, {
-                  text: 'Manage Selected'
-              }, {
-                  text: 'Add a new item'
-              }]
-          }
+      items: [{
+          xtype: 'button',
+          text: 'View',
+          cls: 'x-btn-text-icon',
+          iconCls: 'view-datastream-icon',
+          disabled: true,
+          id: 'viewer-view-file1',
+          handler : function() {
+            var record = selected_node.data.id;
+            ContentModelViewer.functions.selectDatastreamRecord(record);
+            ContentModelViewer.functions.viewSelectedDatastreamRecord();
+          }   
         }
       ]
     }]
